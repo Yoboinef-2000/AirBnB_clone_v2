a8b2e25b6387e6da70e597f39e887651419a8a06
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-04-21 03:04:50 +0300

    Ion want no smoke.

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from hashlib import md5
 from os import getenv
 from sqlalchemy import Column, String
@@ -8,7 +8,7 @@ from sqlalchemy.orm import relationship
 
 theREEEALenv = getenv('HBNB_TYPE_STORAGE')
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     
     if theREEEALenv == "db":
